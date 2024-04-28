48912ceef3bde42c1449f96fd4f1f76ec997c690
delmy <dfajardob@miumg.edu.gt>
2024-04-27 23:11:40 -0600

    Capital total

---
+++ desafio1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace desafio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMontoInicial = new System.Windows.Forms.TextBox();
+            this.txtTasaAnual = new System.Windows.Forms.TextBox();
+            this.txtPeriodoInversion = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(317, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculo del capital final";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Salmon;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(140, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "monto inicial";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Salmon;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(312, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tasa de interes anual";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Salmon;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(487, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Periodo de inversion";
+            // 
+            // txtMontoInicial
+            // 
+            this.txtMontoInicial.BackColor = System.Drawing.Color.Yellow;
+            this.txtMontoInicial.Location = new System.Drawing.Point(131, 120);
+            this.txtMontoInicial.Name = "txtMontoInicial";
+            this.txtMontoInicial.Size = new System.Drawing.Size(76, 20);
+            this.txtMontoInicial.TabIndex = 4;
+            this.txtMontoInicial.Text = "Q.";
+            // 
+            // txtTasaAnual
+            // 
+            this.txtTasaAnual.BackColor = System.Drawing.Color.Yellow;
+            this.txtTasaAnual.Location = new System.Drawing.Point(317, 120);
+            this.txtTasaAnual.Name = "txtTasaAnual";
+            this.txtTasaAnual.Size = new System.Drawing.Size(106, 20);
+            this.txtTasaAnual.TabIndex = 5;
+            this.txtTasaAnual.Text = "%";
+            // 
+            // txtPeriodoInversion
+            // 
+            this.txtPeriodoInversion.BackColor = System.Drawing.Color.Yellow;
+            this.txtPeriodoInversion.Location = new System.Drawing.Point(487, 120);
+            this.txtPeriodoInversion.Name = "txtPeriodoInversion";
+            this.txtPeriodoInversion.Size = new System.Drawing.Size(106, 20);
+            this.txtPeriodoInversion.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Fuchsia;
+            this.button1.Location = new System.Drawing.Point(308, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 43);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Resultado";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BlueViolet;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPeriodoInversion);
+            this.Controls.Add(this.txtTasaAnual);
+            this.Controls.Add(this.txtMontoInicial);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMontoInicial;
+        private System.Windows.Forms.TextBox txtTasaAnual;
+        private System.Windows.Forms.TextBox txtPeriodoInversion;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ desafio1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desafio1
+{
+    public partial class Form1 : Form
+    {
+        decimal TotalCapital = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(txtMontoInicial.Text.ToString());
+            double num2 = double.Parse(txtTasaAnual.Text.ToString())/100;
+            double num3 = double.Parse(txtPeriodoInversion.Text.ToString());
+            double tasa = Math.Pow(1 + num2, num3);
+            double capitalTotal = num1 * tasa;
+            button1 .Text = capitalTotal.ToString();
+
+
+         
+
+
+
+        }
+    }
+}
