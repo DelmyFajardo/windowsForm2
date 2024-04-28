5e9f64a17b040fce2a38b53809cbec43495a4edf
delmy <dfajardob@miumg.edu.gt>
2024-04-27 23:12:20 -0600

    cuotas a pagar

---
+++ desafio2/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace desafio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMontodelPrestamos = new System.Windows.Forms.TextBox();
+            this.txtTasaAnual = new System.Windows.Forms.TextBox();
+            this.txtPlazodelCredito = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(242, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cuotas a pagar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label2.Font = new System.Drawing.Font("Elephant", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(519, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Monto del prestamo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label3.Font = new System.Drawing.Font("Elephant", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(519, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tasa de interes anual";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label4.Font = new System.Drawing.Font("Elephant", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(519, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Plazo del credito";
+            // 
+            // txtMontodelPrestamos
+            // 
+            this.txtMontodelPrestamos.BackColor = System.Drawing.Color.Red;
+            this.txtMontodelPrestamos.Location = new System.Drawing.Point(140, 111);
+            this.txtMontodelPrestamos.Name = "txtMontodelPrestamos";
+            this.txtMontodelPrestamos.Size = new System.Drawing.Size(151, 20);
+            this.txtMontodelPrestamos.TabIndex = 4;
+            // 
+            // txtTasaAnual
+            // 
+            this.txtTasaAnual.BackColor = System.Drawing.Color.Red;
+            this.txtTasaAnual.Location = new System.Drawing.Point(140, 146);
+            this.txtTasaAnual.Name = "txtTasaAnual";
+            this.txtTasaAnual.Size = new System.Drawing.Size(151, 20);
+            this.txtTasaAnual.TabIndex = 5;
+            // 
+            // txtPlazodelCredito
+            // 
+            this.txtPlazodelCredito.BackColor = System.Drawing.Color.Red;
+            this.txtPlazodelCredito.Location = new System.Drawing.Point(140, 183);
+            this.txtPlazodelCredito.Name = "txtPlazodelCredito";
+            this.txtPlazodelCredito.Size = new System.Drawing.Size(151, 20);
+            this.txtPlazodelCredito.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label5.Font = new System.Drawing.Font("Tempus Sans ITC", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(136, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 24);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Cuota a pagar";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Yellow;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(518, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(120, 24);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "                      ";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Fuchsia;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPlazodelCredito);
+            this.Controls.Add(this.txtTasaAnual);
+            this.Controls.Add(this.txtMontodelPrestamos);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMontodelPrestamos;
+        private System.Windows.Forms.TextBox txtTasaAnual;
+        private System.Windows.Forms.TextBox txtPlazodelCredito;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ desafio2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desafio2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            float prestamo = float.Parse(txtMontodelPrestamos.Text.ToString());
+            float interes = float.Parse(txtTasaAnual.Text.ToString()) / 100;
+            float PlazoDelCredito = float.Parse(txtPlazodelCredito.Text.ToString()) * 12;
+            float cuota1 = interes*prestamo;
+           float cuota2 = (float)(1 -Math.Pow(1 + interes, -PlazoDelCredito));
+
+            float cuotaMensual = cuota1/cuota2;
+            label6.Text = cuotaMensual.ToString();
+
+        }
+    }
+}
